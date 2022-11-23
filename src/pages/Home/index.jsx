13d58c2b5ad597a602dafdd9e220a96ee4926cd8
Igor Rocha <igor_roc@hotmail.com.br>
2022-11-23 17:14:32 -0300

    [add]: Home content

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,28 @@
 import React from "react"
+import Botao from "../../components/Botao"
+import Layout from "../../components/Layout"
+import { StyledMain } from "./styles"
+
+import Leaves from "../../assets/leaves.png"
 
 export default function Home() {
-	return <div>Home</div>
+	return (
+		<Layout header="Início">
+			<StyledMain>
+				<h1>Plataforma Cacau</h1>
+				<p>
+					É uma plataforma integrada com sistemas e aplicações que
+					coletam dados durante o período do cultivo do cacau,
+					servindo como ferramenta de análise e prospecção.
+				</p>
+				<div className="botoes">
+					<Botao outline>Faça Login</Botao>
+					<Botao>Saiba mais</Botao>
+				</div>
+				<div className="BG">
+					<img src={Leaves} alt="background" />
+				</div>
+			</StyledMain>
+		</Layout>
+	)
 }
