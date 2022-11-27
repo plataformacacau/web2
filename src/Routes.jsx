c0d428a47d8d5e7b39faed0f40f396f0cbd195f3
Igor Rocha <igor_roc@hotmail.com.br>
2022-11-27 13:08:19 -0300

    feat: Criando inicio da pagina sobre

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
 
 export default function AppRoutes() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename="/web2/">
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/sobre" element={<Sobre />} />
 			</Routes>
 		</BrowserRouter>
 	)
---
+++ src/pages/Sobre/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../../components/Layout"
+
+export default function Sobre() {
+	return (
+		<Layout header="Sobre">
+			<h1>Sobre</h1>
+		</Layout>
+	)
+}
