1bf62259d12d0bcecd8a1df7097c00b328053a3e
Igor Rocha <igor_roc@hotmail.com.br>
2022-11-27 13:08:07 -0300

    feat: Footer finalizado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,61 @@
 import React from "react"
 import { StyledFooter } from "./styles"
 import Logo from "../../assets/logo.png"
+import LogoWhite from "../../assets/logo-branca.png"
+import UESC from "../../assets/uesc.png"
+import ceplac from "../../assets/ceplac.png"
+import ima from "../../assets/ima.png"
+import proex from "../../assets/proex.png"
 
 export default function Footer() {
 	return (
 		<StyledFooter>
 			<div className="MaxWidthWrapper">
 				<div className="FooterContent">
-					<div className="FooterLogo">
-						<img src={Logo} alt="Logo" />
+					<div className="FooterSection info">
+						<a className="logo" href="/">
+							<img src={LogoWhite} alt="Logo" />
+						</a>
+						<p>Plataforma integrada para controle e análise do cultivo do cacau</p>
+					</div>
+					<div className="FooterSection">
+						<h3>Realização</h3>
+						<ul className="realizacao">
+							<li>
+								<a href="https://uesc.br">
+									<img src={UESC} alt="" />
+								</a>
+							</li>
+							<li>
+								<a href="https://ceplac.com.br">
+									<img src={ceplac} alt="" />
+								</a>
+							</li>
+							<li>
+								<a href="https://uesc.br">
+									<img src={ima} alt="" />
+								</a>
+							</li>
+							<li>
+								<a href="https://uesc.br">
+									<img src={proex} alt="" />
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div className="FooterSection sobre">
+						<h3>Sobre nós</h3>
+						<ul>
+							<li>
+								<a href="/">O projeto</a>
+							</li>
+							<li>
+								<a href="/">Quem somos</a>
+							</li>
+							<li>
+								<a href="/">Entre em contato</a>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
@@ -16,8 +63,7 @@ export default function Footer() {
 			<div className="copy">
 				<p>© 2022 Plataforma Cacau. Todos os direitos reservados.</p>
 				<p>
-					Desenvolvido por{" "}
-					<a href="https://ilrocha.com">Igor Rocha</a>
+					Desenvolvido por <a href="https://ilrocha.com">Igor Rocha</a>
 				</p>
 			</div>
 		</StyledFooter>
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,3 +1,120 @@
-import { styled } from "@stitches/react"
+import { keyframes, styled } from "@stitches/react"
 
-export const StyledFooter = styled("footer", {})
+const Animation = keyframes({
+	"0%": { backgroundPositionX: "-100%" },
+	"100%": { backgroundPositionX: "100%" },
+})
+
+export const StyledFooter = styled("footer", {
+	marginTop: "auto",
+	background: "var(--secondary-color-hover)",
+	color: "white",
+	padding: "2em 0",
+
+	".FooterContent": {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "flex-end",
+		gap: "2em",
+		flexWrap: "wrap",
+
+		".logo": {
+			height: "50px",
+			width: "auto",
+		},
+
+		".FooterSection": {
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			alignItems: "center",
+			flex: 1,
+			gap: "1em",
+			width: "max-content",
+
+			ul: {
+				listStyle: "none",
+			},
+
+			".realizacao": {
+				display: "flex",
+				gap: 16,
+			},
+
+			"&.info": {
+				maxWidth: "350px",
+			},
+
+			"&.sobre": {
+				a: {
+					color: "white",
+
+					"&:hover": {
+						color: "var(--primary-color)",
+						fontWeight: "bold",
+					},
+				},
+			},
+		},
+	},
+
+	hr: {
+		margin: "2em 0",
+	},
+
+	".copy": {
+		textAlign: "center",
+		marginBottom: "50px",
+
+		a: {
+			textDecoration: "none",
+			position: "relative",
+			zIndex: 1,
+			display: "inline-block",
+			width: "fit-content",
+			color: "#5aeb73",
+			transition: "all 300ms ease-out",
+
+			"&:after": {
+				content: "",
+				position: "absolute",
+				zIndex: -1,
+				left: 0,
+				bottom: "-1px",
+				width: "100%",
+				height: "1px",
+				background: "linear-gradient(90deg, #5aeb73,#0000, #5aeb73)",
+				backgroundSize: "200% 100%",
+				animation: `${Animation} 1s ease-in-out infinite reverse`,
+			},
+
+			"&:before": {
+				content: "",
+				position: "absolute",
+				zIndex: -1,
+				left: 0,
+				bottom: "-1px",
+				width: "100%",
+				height: "1px",
+				backgroundColor: "#5aeb73",
+				opacity: 0,
+				transition: "all 300ms ease-out",
+			},
+
+			"&:hover": {
+				color: "white",
+				fontWeight: "bold",
+				padding: "0 5px",
+
+				"&:after": {
+					animationPlayState: "paused",
+				},
+				"&:before": {
+					bottom: "0",
+					opacity: 1,
+					height: "100%",
+				},
+			},
+		},
+	},
+})
