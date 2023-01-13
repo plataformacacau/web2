be1b60d998b4952128c4499af1f1a25fb42860f3
Igor Rocha <igor_roc@hotmail.com.br>
2023-01-13 16:26:19 -0300

    fix: Alterar imagem da home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import Botao from "../../components/Botao"
 import Layout from "../../components/Layout"
 import { StyledMain } from "./styles"
 
-import Leaves from "../../assets/leaves.png"
+import Leaves from "../../assets/leaves_dark.png"
 
 export default function Home() {
 	return (
@@ -11,9 +11,8 @@ export default function Home() {
 			<StyledMain>
 				<h1>Plataforma Cacau</h1>
 				<p>
-					É uma plataforma integrada com sistemas e aplicações que
-					coletam dados durante o período do cultivo do cacau,
-					servindo como ferramenta de análise e prospecção.
+					É uma plataforma integrada com sistemas e aplicações que coletam dados durante o
+					período do cultivo do cacau, servindo como ferramenta de análise e prospecção.
 				</p>
 				<div className="botoes">
 					<Botao outline>Faça Login</Botao>
