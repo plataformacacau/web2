95c1023df7a63229b07efd9e8e4dd5b267163be1
Igor Rocha <igor_roc@hotmail.com.br>
2023-01-25 17:53:13 -0300

    fix: Small changes on `Botao`

--- src/components/Botao/styles.js
+++ src/components/Botao/styles.js
@@ -3,13 +3,14 @@ import { styled } from "@stitches/react"
 export const StyledBotao = styled("a", {
 	backgroundColor: "var(--primary-color)",
 	color: "white",
-	padding: "1rem 2rem",
-	borderRadius: 8,
+	padding: "0.5em 1rem",
+	borderRadius: 10,
 	border: "none",
 	cursor: "pointer",
 	fontSize: "1rem",
 	transition: "all 300ms ease-out",
 	fontWeight: "bold",
+	textDecoration: "none",
 
 	"&:hover": {
 		backgroundColor: "var(--primary-color-hover)",
